3c86a994f4317825eda3efdef2c28597529d791f
Andrew <andrew.z.hale1@gmail.com>
2021-01-13 15:52:35 -0800

    fix

--- src/home.js
+++ src/home.js
@@ -5,20 +5,22 @@ import bobashop from "./bobashop.jpg";
 
 function Home() {
   return (
-    <body className="covered">
-      <h1 className="welcome">Welcome to the Tea Shop!</h1>
-      <img className="bobashop" src={bobashop} />
-      <div className="enter">
-        <Link className="btn" to="/order">
-          Order Tea
-        </Link>
-      </div>
-      <div className="enter">
-        <Link className="btn" to="/games">
-          Play Games
-        </Link>
-      </div>
-    </body>
+    <Router>
+      <body className="covered">
+        <h1 className="welcome">Welcome to the Tea Shop!</h1>
+        <img className="bobashop" src={bobashop} />
+        <div className="enter">
+          <Link className="btn" to="/order">
+            Order Tea
+          </Link>
+        </div>
+        <div className="enter">
+          <Link className="btn" to="/games">
+            Play Games
+          </Link>
+        </div>
+      </body>
+    </Router>
   );
 }
 
