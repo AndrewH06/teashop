8e8937894de7b5ee717a83d14351a98ceec9243c
Andrew <andrew.z.hale1@gmail.com>
2021-01-13 16:29:12 -0800

    Router

--- src/home.js
+++ src/home.js
@@ -5,22 +5,20 @@ import bobashop from "./bobashop.jpg";
 
 function Home() {
   return (
-    <Router>
-      <body className="covered">
-        <h1 className="welcome">Welcome to the Tea Shop!</h1>
-        <img className="bobashop" src={bobashop} alt="background" />
-        <div className="enter">
-          <Link className="btn" to="/order">
-            Order Tea
-          </Link>
-        </div>
-        <div className="enter">
-          <Link className="btn" to="/games">
-            Play Games
-          </Link>
-        </div>
-      </body>
-    </Router>
+    <body className="covered">
+      <h1 className="welcome">Welcome to the Tea Shop!</h1>
+      <img className="bobashop" src={bobashop} alt="background" />
+      <div className="enter">
+        <Link className="btn" to="/order">
+          Order Tea
+        </Link>
+      </div>
+      <div className="enter">
+        <Link className="btn" to="/games">
+          Play Games
+        </Link>
+      </div>
+    </body>
   );
 }
 
