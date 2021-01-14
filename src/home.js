02be43dd557c3af45d80b1d98f0b2709e15e8981
Andrew <andrew.z.hale1@gmail.com>
2021-01-13 16:05:23 -0800

    router

--- src/home.js
+++ src/home.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./home.css";
 import bobashop from "./bobashop.jpg";
 
 function Home() {
   return (
-    <Router>
-      <body className="covered">
-        <h1 className="welcome">Welcome to the Tea Shop!</h1>
-        <img className="bobashop" src={bobashop} alt="background" />
-        <div className="enter">
-          <Link className="btn" to="/order">
-            Order Tea
-          </Link>
-        </div>
-        <div className="enter">
-          <Link className="btn" to="/games">
-            Play Games
-          </Link>
-        </div>
-      </body>
-    </Router>
+    <body className="covered">
+      <h1 className="welcome">Welcome to the Tea Shop!</h1>
+      <img className="bobashop" src={bobashop} alt="background" />
+      <div className="enter">
+        <Link className="btn" to="/order">
+          Order Tea
+        </Link>
+      </div>
+      <div className="enter">
+        <Link className="btn" to="/games">
+          Play Games
+        </Link>
+      </div>
+    </body>
   );
 }
 
